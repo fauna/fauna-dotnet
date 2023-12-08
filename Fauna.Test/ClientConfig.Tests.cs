c1d1bc15fef65aa920fc64dddf54ba7293d842c7
Adam Bollen <adam.bollen@fauna.com>
2023-12-07 16:02:38 -0800

    New QueryOptions class and test

--- Fauna.Test/ClientConfig.Tests.cs
+++ Fauna.Test/ClientConfig.Tests.cs
@@ -13,4 +13,30 @@ public class ClientConfigTests
         Assert.AreEqual("secret", b.Secret);
         Assert.AreEqual(Constants.Endpoints.Default, b.Endpoint);
     }
+
+    [Test]
+    public void FinalQueryOptions()
+    {
+        var defaults = new QueryOptions
+        {
+            TypeCheck = true,
+            QueryTags = new Dictionary<string, string> {{"foo", "bar"}, {"baz", "luhrmann"}},
+            QueryTimeout = TimeSpan.FromSeconds(30)
+        };
+        var overrides = new QueryOptions
+        {
+            Linearized = true,
+            QueryTags = new Dictionary<string, string> {{"foo", "yep"}}
+        };
+
+        var finalOptions = QueryOptions.GetFinalQueryOptions(defaults, overrides);
+
+        Assert.IsNotNull(finalOptions);
+        Assert.IsTrue(finalOptions!.Linearized);
+        Assert.IsTrue(finalOptions!.TypeCheck);
+        Assert.IsNull(finalOptions!.TraceParent);
+        Assert.IsNotNull(finalOptions!.QueryTags);
+        Assert.AreEqual("yep", finalOptions!.QueryTags!["foo"]);
+        Assert.AreEqual("luhrmann", finalOptions!.QueryTags!["baz"]);
+    }
 }
