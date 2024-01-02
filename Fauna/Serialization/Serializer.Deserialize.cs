f4557ace3aef3df6350153a156ae6652093744f5
Matt Freels <matt@freels.name>
2024-01-02 15:58:15 -0700

    make explicit context deserialize public

--- Fauna/Serialization/Serializer.Deserialize.cs
+++ Fauna/Serialization/Serializer.Deserialize.cs
@@ -20,7 +20,7 @@ public static partial class Serializer
         var reader = new Utf8FaunaReader(str);
         var context = new SerializationContext();
         reader.Read();
-        var obj = DeserializeValueInternal(ref reader, context, type);
+        var obj = Deserialize(context, ref reader, type);
 
         if (reader.Read())
         {
@@ -30,12 +30,12 @@ public static partial class Serializer
         return obj;
     }
 
-    private static T DeserializeValueInternal<T>(ref Utf8FaunaReader reader, SerializationContext context)
+    public static T Deserialize<T>(SerializationContext context, ref Utf8FaunaReader reader)
     {
-        return (T)DeserializeValueInternal(ref reader, context, typeof(T))!;
+        return (T)Deserialize(context, ref reader, typeof(T))!;
     }
 
-    private static object? DeserializeValueInternal(ref Utf8FaunaReader reader, SerializationContext context, Type? targetType = null)
+    public static object? Deserialize(SerializationContext context, ref Utf8FaunaReader reader, Type? targetType = null)
     {
         var value = reader.CurrentTokenType switch
         {
@@ -84,19 +84,19 @@ public static partial class Serializer
                 switch (fieldName)
                 {
                     case "id":
-                        id = DeserializeValueInternal<string>(ref reader, context);
+                        id = Deserialize<string>(context, ref reader);
                         allProps["id"] = id;
                         break;
                     case "name":
-                        name = DeserializeValueInternal<string>(ref reader, context);
+                        name = Deserialize<string>(context, ref reader);
                         allProps["name"] = name;
                         break;
                     case "coll":
-                        coll = DeserializeValueInternal<Module>(ref reader, context);
+                        coll = Deserialize<Module>(context, ref reader);
                         allProps["coll"] = coll;
                         break;
                     default:
-                        allProps[fieldName] = DeserializeValueInternal(ref reader, context);
+                        allProps[fieldName] = Deserialize(context, ref reader);
                         break;
                 }
             }
@@ -150,19 +150,19 @@ public static partial class Serializer
                 switch (fieldName)
                 {
                     case "id":
-                        id = DeserializeValueInternal<string>(ref reader, context);
+                        id = Deserialize<string>(context, ref reader);
                         break;
                     case "name":
-                        name = DeserializeValueInternal<string>(ref reader, context);
+                        name = Deserialize<string>(context, ref reader);
                         break;
                     case "ts":
-                        ts = DeserializeValueInternal<DateTime>(ref reader, context);
+                        ts = Deserialize<DateTime>(context, ref reader);
                         break;
                     case "coll":
-                        coll = DeserializeValueInternal<Module>(ref reader, context);
+                        coll = Deserialize<Module>(context, ref reader);
                         break;
                     default:
-                        data[fieldName] = DeserializeValueInternal(ref reader, context);
+                        data[fieldName] = Deserialize(context, ref reader);
                         break;
                 }
             }
@@ -211,7 +211,7 @@ public static partial class Serializer
             var lst = new List<object?>();
             while (reader.Read() && reader.CurrentTokenType != TokenType.EndArray)
             {
-                lst.Add(DeserializeValueInternal(ref reader, context));
+                lst.Add(Deserialize(context, ref reader));
             }
             return lst;
         }
@@ -225,7 +225,7 @@ public static partial class Serializer
             {
                 var parameters = new[]
                 {
-                    DeserializeValueInternal(ref reader, context, elementType)
+                    Deserialize(context, ref reader, elementType)
                 };
                 add.Invoke(lst, parameters);
             }
@@ -260,7 +260,7 @@ public static partial class Serializer
 
                 if (fieldMap.ContainsKey(fieldName))
                 {
-                    fieldMap[fieldName].Info!.SetValue(instance, DeserializeValueInternal(ref reader, context)!);
+                    fieldMap[fieldName].Info!.SetValue(instance, Deserialize(context, ref reader)!);
                 }
                 else
                 {
@@ -289,7 +289,7 @@ public static partial class Serializer
                 {
                     var fieldName = reader.GetString()!;
                     reader.Read();
-                    obj[fieldName] = DeserializeValueInternal(ref reader, context)!;
+                    obj[fieldName] = Deserialize(context, ref reader)!;
                 }
                 else
                     throw new SerializationException(
@@ -327,7 +327,7 @@ public static partial class Serializer
                     var parameters = new[]
                     {
                         fieldName,
-                        DeserializeValueInternal(ref reader, context, valueType)
+                        Deserialize(context, ref reader, valueType)
                     };
                     add.Invoke(obj, parameters);
                 }
