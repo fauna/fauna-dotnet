1954e5e31c80624a03a94a5253cd7fbc3a4cb9af
Matt Freels <matt@freels.name>
2024-01-30 18:19:45 -0700

    add test fixture setup helpers

---
+++ Fauna.Test/Helpers/Fixtures.cs
@@ -0,0 +1,43 @@
+using Fauna.Mapping.Attributes;
+using static Fauna.Query;
+
+namespace Fauna.Test;
+
+[Object]
+public class Author
+{
+    [Field] public string? Id { get; set; }
+    [Field] public string Name { get; set; } = "Alice";
+}
+
+public class AuthorDb : DataContext
+{
+    public class AuthorCol : Collection<Author>
+    {
+        public Index<Author> ByName(string name) => Index().Call(name);
+    }
+
+    public AuthorCol Author { get => GetCollection<AuthorCol>(); }
+}
+
+public static class Fixtures
+{
+    public static AuthorDb AuthorDb(Client client)
+    {
+        client.QueryAsync(FQL("Collection.byName('Author')?.delete()")).Wait();
+        client.QueryAsync(FQL(
+            @"Collection.create({
+                name: 'Author',
+                indexes: {
+                    byName: {
+                        terms: [{ field: '.name'}]
+                    }
+                }
+            })"))
+            .Wait();
+        client.QueryAsync(FQL("Author.create({name: 'Alice'})")).Wait();
+        client.QueryAsync(FQL("Author.create({name: 'Bob'})")).Wait();
+
+        return client.DataContext<AuthorDb>();
+    }
+}
