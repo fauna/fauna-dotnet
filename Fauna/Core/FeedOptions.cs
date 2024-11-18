f6142aa3adc431138c22b13b26cc6aba417c016b
James Rodewig <james.rodewig@fauna.com>
2024-11-18 16:22:31 -0500

    [DOCS-3753] Event Feeds: Document `pageSize` limit (#228)

--- Fauna/Core/FeedOptions.cs
+++ Fauna/Core/FeedOptions.cs
@@ -11,7 +11,8 @@ public class FeedOptions : EventOptions
     /// Initializes a new instance of the <see cref="FeedOptions"/> class with the specified cursor and optional page size.
     /// </summary>
     /// <param name="cursor">The cursor for the feed. Used to resume the Feed.</param>
-    /// <param name="pageSize">Optional page size for the feed.</param>
+    /// <param name="pageSize">Optional page size for the feed. Sets the maximum number of events returned per page. Must
+    /// be in the range 1 to 16000 (inclusive). Defaults to 16.</param>
     /// <seealso href="https://docs.fauna.com/fauna/current/reference/cdc/#get-events-after-a-specific-cursor"/>
     public FeedOptions(string cursor, int? pageSize = null)
     {
@@ -23,7 +24,8 @@ public class FeedOptions : EventOptions
     /// Initializes a new instance of the <see cref="FeedOptions"/> class with the specified start timestamp and optional page size.
     /// </summary>
     /// <param name="startTs">The start timestamp for the feed. Used to resume the Feed.</param>
-    /// <param name="pageSize">Optional page size for the feed.</param>
+    /// <param name="pageSize">Optional page size for the feed. Sets the maximum number of events returned per page. Must
+    /// be in the range 1 to 16000 (inclusive). Defaults to 16.</param>
     /// <seealso href="https://docs.fauna.com/fauna/current/reference/cdc/#get-events-after-a-specific-cursor"/>
     public FeedOptions(long startTs, int? pageSize = null)
     {
@@ -34,7 +36,8 @@ public class FeedOptions : EventOptions
     /// <summary>
     /// Initializes a new instance of the <see cref="FeedOptions"/> class with the specified page size.
     /// </summary>
-    /// <param name="pageSize">The page size for the feed.</param>
+    /// <param name="pageSize">Maximum number of events returned per page. Must
+    /// be in the range 1 to 16000 (inclusive). Defaults to 16.</param>
     public FeedOptions(int pageSize)
     {
         PageSize = pageSize;
